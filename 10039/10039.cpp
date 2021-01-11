0e1ed3ed3e601b06185625b5ddf3273eb5c4175e
h0han <hyhan@dgist.ac.kr>
2021-01-11 10:40:24 +0900

    평균 점수

---
+++ 10039/10039.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int st1, st2, st3, st4, st5;
+	scanf("%d\n%d\n%d\n%d\n%d", &st1, &st2, &st3, &st4, &st5);
+	int arr[5] = [st1, st2, st3, st4, st5];
+	int sum = st1 + st2 + st3 + st4 + st5;
+	while(i == 5){
+		if (arr[i] < 40)
+			arr[i] = 40;
+			i++		
+	}
+	printf("%d", sum/5);
+}
---
+++ 10039/10039.py
@@ -0,0 +1,3 @@
+st1, st2, st3, st4, st5 = map(int, input().split())
+sum = st1 + st2 + st3 + st4 + st5
+print(sum/5)
